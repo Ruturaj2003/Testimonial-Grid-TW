9850c81d2e41352cecd30541eaad03b73dab5bb1
Ruturaj <ruturajhb2003@gmail.com>
2023-06-14 19:31:35 +0530

    Add First Box

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./*.html'],
   screens: {
@@ -13,5 +14,6 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/line-clamp')],
+  plugins: [],
 }
+
